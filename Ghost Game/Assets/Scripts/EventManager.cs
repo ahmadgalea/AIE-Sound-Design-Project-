1b8020279d878856fe06156b2ca84092b5c561b1
ahmadgalea <ahmadgalea@gmail.com>
2021-09-16 11:31:21 +1000

    Updated HauntedObject script
    Fleshed out Haunted object script and events.

--- Ghost Game/Assets/Scripts/EventManager.cs
+++ Ghost Game/Assets/Scripts/EventManager.cs
@@ -9,6 +9,12 @@ public class EventManager : MonoBehaviour
     public static event LightAction OnLightOn;
     public static event LightAction OnLightOff;
 
+    public delegate void PossessionAction(Room room, ObjectType type);
+
+    public static event PossessionAction OnPossessionStart;
+    public static event PossessionAction OnPossessionStop;
+    public static event PossessionAction OnPossessionComplete;
+
     // rooms: 0-kitchen, 1-dining, 2-bathroom
     public static void TurnOnLight(int room)
     {
@@ -33,6 +39,30 @@ public class EventManager : MonoBehaviour
         if (EventManager.OnLightOff != null)
         {
             EventManager.OnLightOff(room);
+        }
+    }
+
+    public static void PossessionSound(Room room,ObjectType type)
+    {
+        if (EventManager.OnPossessionStart != null)
+        {
+            EventManager.OnPossessionStart(room,type);
+        }
+    }
+
+    public static void StopPossession(Room room, ObjectType type)
+    {
+        if (EventManager.OnPossessionStop != null)
+        {
+            EventManager.OnPossessionStop(room,type);
+        }
+    }
+
+    public static void PossessionComplete(Room room, ObjectType type)
+    {
+        if (EventManager.OnPossessionComplete != null)
+        {
+            EventManager.OnPossessionComplete(room, type);
         }
     }
 
--- Ghost Game/Assets/Scripts/HauntedObject.cs
+++ Ghost Game/Assets/Scripts/HauntedObject.cs
@@ -14,16 +14,55 @@ public class HauntedObject : MonoBehaviour
     public ObjectType type;
     public Room room;
 
+    public float possessionTimeLimit = 5.0f;
+
     private AudioSource audio = null;
+
+    private float possessionTimer = 0.0f;
+    private bool ghostPossessing = false;
+    private bool isPossessed = false;
 
     void Start()
     {
+        EventManager.OnPossessionStart += OnPossessionStart;
+        EventManager.OnPossessionStop += OnPossessionStop;
+        EventManager.OnPossessionComplete += OnPossessionComplete;
         audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(!isPossessed && ghostPossessing)
+        {
+            possessionTimer += Time.deltaTime;
+            if(possessionTimer >= possessionTimeLimit)
+            {
+                EventManager.PossessionComplete(room, type);
+            }
+        }
+    }
+    private void OnPossessionStart(Room room, ObjectType type)
+    {
+        isPossessed = false;
+        ghostPossessing = true;
+        possessionTimer = 0.0f;
+    }
+
+    private void OnPossessionStop(Room room, ObjectType type)
+    {
+        isPossessed = false;
+        ghostPossessing = false;
+        possessionTimer = 0.0f;
+    }
+
+    private void OnPossessionComplete(Room room, ObjectType type)
+    {
+        isPossessed = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EventManager.StopPossession(room, type);
     }
 }

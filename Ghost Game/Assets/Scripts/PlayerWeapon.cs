3f1a2cfba847568acf461fd457d4ec03ff2026ca
ahmadgalea <ahmadgalea@gmail.com>
2021-09-20 13:22:03 +1000

    Removed Gizmo

--- Ghost Game/Assets/Scripts/PlayerWeapon.cs
+++ Ghost Game/Assets/Scripts/PlayerWeapon.cs
@@ -59,9 +59,4 @@ public class PlayerWeapon : MonoBehaviour
             EventManager.TurnOnLight(highlighedSwitch.room);
         }
     }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.DrawLine(transform.position, transform.position-10*transform.forward);
-    }
 }
